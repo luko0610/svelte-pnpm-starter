df04266e6af2fed37bad25e904c11188bd4f7e4e
Lukas Kohlmaier <luko0610@gmail.com>
2023-10-31 20:46:15 +0100

    FIX: Ran formatter

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,20 +1,20 @@
 <script>
-  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
 </script>
 
 <Navbar let:hidden let:toggle>
-  <NavBrand href="/">
-    <img
-      src="https://placehold.co/600x300"
-      class="mr-3 h-6 sm:h-9"
-      alt="remix-ts-flowbite-starter"
-    />
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-      remix-ts-flowbite-starter
-    </span>
-  </NavBrand>
-  <NavHamburger on:click={toggle} />
-  <NavUl {hidden}>
-    <NavLi href="/" active={true}>Home</NavLi>
-  </NavUl>
+	<NavBrand href="/">
+		<img
+			src="https://placehold.co/600x300"
+			class="mr-3 h-6 sm:h-9"
+			alt="remix-ts-flowbite-starter"
+		/>
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			remix-ts-flowbite-starter
+		</span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi href="/" active={true}>Home</NavLi>
+	</NavUl>
 </Navbar>
